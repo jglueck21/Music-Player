3a8883dfada3040472ef676ea6a4f5702602ff67
jglueck21 <jglueck@student.tgm.ac.at>
2025-02-15 19:56:46 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const songs = [
+  {
+    id: 0,
+    title: "Machinery of War",
+    artist: "Evgeny Bardyuzha",
+    image:
+      "https://res.cloudinary.com/tropicolx/image/upload/v1675352152/music_app/machinery-of-war_fqu8z6.jpg",
+    src: "https://res.cloudinary.com/tropicolx/video/upload/v1675218409/music_app/Evgeny_Bardyuzha_-_Machinery_of_War_fyaroh.mp3",
+  },
+];
+
+function App() {
+  return <></>;
+}
+
+export default App;
